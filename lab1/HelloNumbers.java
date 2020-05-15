39d1d2ff07bc55e67c82fa94cbc40824d9f06892
Rosen-zrs <1018477962@qq.com>
2020-05-15 10:09:06 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers{
+    public static void main(String[] args){
+        int x = 0,i = 1;
+        while(x <= 45){
+            System.out.print(x + " ");
+            x += i++;
+        }
+    }
+}
+
+
+
