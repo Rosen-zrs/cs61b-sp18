7ffe6ac0cfe16df0d9fae6c1685e45000760e73f
Rosen-zrs <1018477962@qq.com>
2020-05-16 15:44:09 +0800

    resubmit proj0

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,6 +1,6 @@
 public class Planet {
 
-    public static double G = 6.67e-11;
+    private static double G = 6.67e-11;
     public double xxPos, yyPos, xxVel, yyVel, mass;
     public String imgFileName;
 
